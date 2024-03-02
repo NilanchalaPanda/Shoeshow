9d31c398c512ecd2f040c357e2db44fe8a3a3dbb
Nilanchal <nilanchalpanda2003@gmail.com>
2024-03-02 17:47:36 +0530

    Products section added

---
+++ app/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <Link
+      href="/product/1"
+      className="transform overflow-hidden bg-white duration-200 hover:scale-[1.03] cursor-pointer"
+    >
+      <img src="/product.jpg" alt="SHOE IMAGE" className="rounded-xl"/>
+      <div className="p-4 text-black/[0.9]">
+        <h2 className="text-lg font-medium">Product Name</h2>
+        <div className="flex items-center text-black/[0.5]">
+          <p className="mr-3 text-lg font-semibold">$20.00</p>
+          <p className="text-base font-medium line-through">$25.00</p>
+          <p className="ml-auto text-base text-green-400">20% off</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,38 @@
-import Image from "next/image";
+import HeroBanner from "./components/HeroBanner";
+import ProductCard from "./components/ProductCard";
+import Wrapper from "./components/Wrapper";
 
 export default function Home() {
   return (
-    <>
-      <h1>HELLO</h1>
-    </>
+    <main>
+      <HeroBanner />
+      <Wrapper>
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Cushioning for your Miles
+          </div>
+          <div className="text-md md:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit est
+            odio eligendi quasi quo magnam velit, illo quas perferendis
+            distinctio, cupiditate asperiores dignissimos minima provident! Modi
+            enim, maiores laudantium nisi illum exercitationem eaque asperiores
+            magni?
+          </div>
+        </div>
+
+        {/* Product listed */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </Wrapper>
+    </main>
   );
 }
